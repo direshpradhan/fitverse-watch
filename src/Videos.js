00f18a7e9611737aa3283bc45049ff1802701853
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-04-07 13:22:36 +0530

    display videos list

--- src/Videos.js
+++ src/Videos.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {videos} from "./videosDB";
+
 
 export const Videos = () => {
     return (
         <div>
             <h1>This is video page.</h1>
+            <div style={{display:"flex",flexWrap:"wrap",justifyContent:"space-evenly"}}>
+                {videos.map(video=>{
+                    return (
+                        <div style={{margin:"1rem 0",width:"30%"}}>
+                            <img width="95%" src={video.image} />
+                            <div style={{alignText:"center",width:"90%",margin:"0 auto"}}>{video.title}</div> 
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
--- src/videosDB.js
+++ src/videosDB.js
@@ -1,38 +1,74 @@
-export const data = [
+export const videos = [
   {
     id: 1,
-    name: 'Best Home Workout Routine | BeerBiceps Fitness',
+    title: 'Best Home Workout Routine | BeerBiceps Fitness',
     image:'https://img.youtube.com/vi/dI0O8TnC2d8/hqdefault.jpg',
     url: 'https://www.youtube.com/watch?v=dI0O8TnC2d8',
   },
   {
     id: 2,
-    name: 'Guided Home Workout for Men and Women| BeerBiceps Fitness',
+    title: 'Guided Home Workout for Men and Women| BeerBiceps Fitness',
     image:'https://img.youtube.com/vi/AXZlb-3MMYE/hqdefault.jpg',
     url: 'https://www.youtube.com/watch?v=AXZlb-3MMYE',
   },
   {
     id: 3,
-    name: 'CHEST Workout for Men & Women | Beginners Chest Exercises without equipment | BeerBiceps',
+    title: 'CHEST Workout for Men & Women | Beginners Chest Exercises without equipment | BeerBiceps',
     image:'https://img.youtube.com/vi/NK-UIRH-K_k/hqdefault.jpg',
     url: 'https://www.youtube.com/watch?v=NK-UIRH-K_k',
   },
   {
     id: 4,
-    name: 'Home Workout series - BACK WORKOUT for MEN & WOMEN | Exercise FORM & Training plan',
+    title: 'Home Workout series - BACK WORKOUT for MEN & WOMEN | Exercise FORM & Training plan',
     image:'https://img.youtube.com/vi/TvbRwuAAsxQ/hqdefault.jpg',
     url: 'https://www.youtube.com/watch?v=TvbRwuAAsxQ',
   },
   {
     id: 5,
-    name: 'AT HOME Leg Workout for Men & Women | Exercise Form & Training Plan | BeerBiceps Home Workout Series',
+    title: 'AT HOME Leg Workout for Men & Women | Exercise Form & Training Plan | BeerBiceps Home Workout Series',
     image:'https://img.youtube.com/vi/_sCM0q_OTAA/hqdefault.jpg',
     url: 'https://www.youtube.com/watch?v=_sCM0q_OTAA',
   },
   {
     id: 6,
-    name: 'Arm Workout AT HOME For Men & Women | Exercise Form & Training Plan | BeerBiceps Home Workout Series',
+    title: 'Home Workout||Full Body Workout At Home|| No Gym',
     image:'https://img.youtube.com/vi/Qp88IAYnvWc/hqdefault.jpg',
-    url: 'https://www.youtube.com/watch?Qp88IAYnvWc',
+    url: 'https://www.youtube.com/watch?v=Qp88IAYnvWc',
+  },
+  {
+    id: 7,
+    title: 'Arm Workout AT HOME For Men & Women | Exercise Form & Training Plan | BeerBiceps Home Workout Series',
+    image:'https://img.youtube.com/vi/DOB9vG6KRBM/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=DOB9vG6KRBM',
+  },
+  {
+    id: 8,
+    title: '20 Minute Desi Workout For Full Body|Full Body Home Workout|20 मिनट में घर पर करें फुल बौडी वकॉउट ||',
+    image:'https://img.youtube.com/vi/g___5hT_gvw/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=g___5hT_gvw',
+  },
+  {
+    id: 9,
+    title: 'Top5 Side Fat(Love Handles) Home Workout |How to Remove Love Handles|5Min Home Workout For Side Fat',
+    image:'https://img.youtube.com/vi/EY1WJ8FuIpo/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=EY1WJ8FuIpo',
+  },
+  {
+    id: 10,
+    title: 'V cut Abs Workout At Home|बिना Gym घर पर बनाऐं Abs|| Glutone 250mg||Rajveer Shishodia',
+    image:'https://img.youtube.com/vi/FfP_bGvpKTU/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=FfP_bGvpKTU',
+  },
+  {
+    id: 11,
+    title: 'Arm Workout AT HOME For Men & Women | Exercise Form & Training Plan | BeerBiceps Home Workout Series',
+    image:'https://img.youtube.com/vi/Qp88IAYnvWc/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=Qp88IAYnvWc',
+  },
+  {
+    id: 12,
+    title: 'Arm Workout AT HOME For Men & Women | Exercise Form & Training Plan | BeerBiceps Home Workout Series',
+    image:'https://img.youtube.com/vi/Qp88IAYnvWc/hqdefault.jpg',
+    url: 'https://www.youtube.com/watch?v=Qp88IAYnvWc',
   },
 ];
