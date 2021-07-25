8605e822c1e62d0e5ebac4e415b276ca28206c31
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-07-25 23:08:53 +0530

    style: added styling to library page

--- src/pages/Library/Library.js
+++ src/pages/Library/Library.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { BottomNav } from "../../components/BottomNav/BottomNav";
-// import styles from "./Library.module.css";
+import styles from "./Library.module.css";
 
 export const Library = () => {
   const navigate = useNavigate();
   return (
     <div>
       <ul>
-        <li onClick={() => navigate("/history")}>History</li>
-        <li onClick={() => navigate("/playlist")}>Playlist</li>
-        <li onClick={() => navigate("/watch-later")}>Watch Later</li>
-        <li onClick={() => navigate("/liked-videos")}>Liked Videos</li>
+        <li
+          className={`${styles.listitem} flex pointer`}
+          onClick={() => navigate("/history")}
+        >
+          <span class="material-icons-outlined">history</span> History
+        </li>
+        <li
+          className={`${styles.listitem} flex pointer`}
+          onClick={() => navigate("/playlist")}
+        >
+          <span class="material-icons">playlist_play</span> Playlist
+        </li>
+        <li
+          className={`${styles.listitem} flex pointer`}
+          onClick={() => navigate("/watch-later")}
+        >
+          <span class="material-icons-outlined">watch_later</span> Watch Later
+        </li>
+        <li
+          className={`${styles.listitem} flex pointer`}
+          onClick={() => navigate("/liked-videos")}
+        >
+          <span class="material-icons-outlined">thumb_up</span> Liked Videos
+        </li>
       </ul>
       <BottomNav />
     </div>
