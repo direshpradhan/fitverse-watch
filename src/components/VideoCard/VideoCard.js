b3e4852823c1b05b177f18b6e05a2c8ba0c8f7ba
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-07-26 13:49:26 +0530

    feat: added more options on videocard

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useData } from "../../context/DataContext";
-import { addToHistory } from "../../services";
-import { MoreOptions } from "../MoreOptions/MoreOptions";
+import {
+  addToHistory,
+  addToLikedVideos,
+  addToWatchLater,
+  removeFromLikedVideos,
+  removeFromWatchLater,
+} from "../../services";
 import styles from "./VideoCard.module.css";
 
 export const VideoCard = ({ videoId }) => {
-  const { dispatch, videos } = useData();
+  const { dispatch, videos, watchLater, likedVideos } = useData();
   const [showOptionsModal, setShowOptionsModal] = useState(false);
   const video = videos.find((video) => video._id === videoId);
   const {
@@ -18,14 +23,18 @@ export const VideoCard = ({ videoId }) => {
     publishedOn,
     statistics: { viewCount },
   } = video;
+  const isInWatchLater = watchLater.find((videoID) => videoID === id);
+  const isInLikedVideos = likedVideos.find((videoID) => videoID === id);
   const navigate = useNavigate();
 
   return (
-    // <Link className={`${styles.link}`} to={`/video/${id}`}>
     <>
       <div
         className={`${styles.main}`}
-        onClick={() => addToHistory(video._id, dispatch)}
+        onClick={() => {
+          addToHistory(video._id, dispatch);
+          showOptionsModal && setShowOptionsModal(() => false);
+        }}
       >
         <img
           className={`${styles.image}`}
@@ -54,15 +63,53 @@ export const VideoCard = ({ videoId }) => {
         </div>
         <span
           onClick={() => setShowOptionsModal((option) => !option)}
-          className={`${styles.more_options_icon} material-icons-outlined`}
+          className={`${styles.more_options_icon} material-icons-outlined pointer`}
         >
           more_vert
         </span>
         <div className={`${styles.more_options}`}>
-          {showOptionsModal && <MoreOptions id={id} />}
+          {showOptionsModal && (
+            <div className={`${styles.container}`}>
+              {isInWatchLater ? (
+                <div
+                  className="pointer"
+                  onClick={() => removeFromWatchLater(id, dispatch)}
+                >
+                  Remove From Watch Later
+                </div>
+              ) : (
+                <div
+                  className="pointer"
+                  onClick={() => addToWatchLater(id, dispatch)}
+                >
+                  Add to Watch Later
+                </div>
+              )}
+              {isInLikedVideos ? (
+                <div
+                  onClick={() => removeFromLikedVideos(id, dispatch)}
+                  className="pointer"
+                >
+                  Remove From Liked Videos
+                </div>
+              ) : (
+                <div
+                  onClick={() => addToLikedVideos(id, dispatch)}
+                  className="pointer"
+                >
+                  Add to Liked Videos
+                </div>
+              )}
+              <div
+                className={`pointer`}
+                onClick={() => setShowOptionsModal(() => false)}
+              >
+                Cancel
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
-    // </Link>
   );
 };
